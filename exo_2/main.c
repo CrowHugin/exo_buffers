3dcfbeaa4ad8b0a3808d3f82ca9e211ffc560def
CrowHugin <kuntz.bastien@gmail.com>
2026-02-04 22:36:34 +0100

    Made the script better for ex2

--- exo_2/main.c
+++ exo_2/main.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 
 int main(void){
-  int n=0, i,ind;
+  int n=0, i, j=0;
 
   char buffer[50];
 
   printf("Veuillez rentrer une phrase svp\n");
   fgets(buffer, sizeof(buffer),stdin);
 
-  while(buffer[n] != '\n' && buffer[n] != '\0'){
-       n++;
-       }
-
-  for (i=0;i<n;i++)
-  {
-    if (buffer[i] == ' ')
+  while(buffer[n] != '\n' && buffer[n] != '\0')
     {
-      for (ind=i; ind<n; ind++)
-      {
-        buffer[ind]=buffer[ind+1];
-      }
+      n++;
+    }
+
+  for(i=0;i<n;i++)
+  {
+
+    if(buffer[i] != ' ' && buffer[i] != '\n')
+    {
+      buffer[j] = buffer[i];
+      j++;
     }
   }
+
+  buffer[j] = '\0';
   printf("%s\n", buffer);
 
 
